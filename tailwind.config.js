fd2e2c335e8fff300c2ce23a57f814676c4ab2da
Camilo Portilla <cp1085275@gmail.com>
2023-07-08 19:21:18 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding:"1rem",
+
+    },
+    fontFamily:{
+      Poppins:["Poppins, sans-serif"]},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
+  },
+  plugins: [],
+}
+
